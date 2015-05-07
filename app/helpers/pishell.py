3d4997b64712ee4738bc123f313ae3de3ce6a6d4
Alix Fullerton <alix@politeauthority.com>
2015-05-06 18:45:04 -0600

    Updates to PiShell

--- app/helpers/pishell.py
+++ app/helpers/pishell.py
@@ -16,30 +16,43 @@ class PiShell(object):
         out, err = p.communicate()
         response = out.split('\n')
 
-        interfaces = []
-        c          = 0
+
+        raw_interfaces = []
         for r in response:
             if 'eth0' in r or 'wlan0' in r:
-                interface = {}
-                print r
-                interface['mac'] = response[c].split('HWaddr')[1].strip()
-                interface['ip']  = response[c+1].split('Bcast')[0].split('addr:')[1].strip()
-                interface['rx']  = {
-                    'name'      : '',
-                    'packets'   : response[c+4].split('packets:')[1].split(' errors')[0].strip(),
-                    'errors'    : response[c+4].split('errors:')[1].split(' dropped')[0].strip(),
-                    'dropped'   : response[c+4].split('dropped:')[1].split(' overruns')[0].strip(),
-                    'bytes_raw' : response[c+7].split('RX bytes:')[1].split('(')[0].strip()
-                }
-                interface['tx']  = {
-                    'name'      : '',
-                    'packets'   : response[c+5].split('packets:')[1].split(' errors')[0].strip(),
-                    'errors'    : response[c+5].split('errors:')[1].split(' dropped')[0].strip(),
-                    'dropped'   : response[c+5].split('dropped:')[1].split(' overruns')[0].strip(),
-                    'bytes_raw' : response[c+7].split('TX bytes:')[1].split('(')[0].strip()
-                }
-                response[c+5]
-                interfaces.append( interface )
+                i_lines = []
+                t = 0
+                while True:
+                    if response[t].strip() == '':
+                        break
+                    i_lines.append( response[t] )
+                    t += 1
+                raw_interfaces.append( i_lines )
+
+        interfaces = []
+        c          = 0
+        for i in raw_interfaces:
+            interface        = {}
+            print i
+            interface['mac'] = self.__find_segment( interface, 'HWaddr')
+            # interface['mac'] = response[c].split('HWaddr')[1].strip()
+            interface['ip']  = response[c+1].split('Bcast')[0].split('addr:')[1].strip()
+            interface['rx']  = {
+                'name'      : '',
+                'packets'   : response[c+4].split('packets:')[1].split(' errors')[0].strip(),
+                'errors'    : response[c+4].split('errors:')[1].split(' dropped')[0].strip(),
+                'dropped'   : response[c+4].split('dropped:')[1].split(' overruns')[0].strip(),
+                'bytes_raw' : response[c+7].split('RX bytes:')[1].split('(')[0].strip()
+            }
+            interface['tx']  = {
+                'name'      : '',
+                'packets'   : response[c+5].split('packets:')[1].split(' errors')[0].strip(),
+                'errors'    : response[c+5].split('errors:')[1].split(' dropped')[0].strip(),
+                'dropped'   : response[c+5].split('dropped:')[1].split(' overruns')[0].strip(),
+                'bytes_raw' : response[c+7].split('TX bytes:')[1].split('(')[0].strip()
+            }
+            response[c+5]
+            interfaces.append( interface )
             c += 1
         return interfaces
 
@@ -48,5 +61,13 @@ class PiShell(object):
             uptime_seconds = float(f.readline().split()[0])
             uptime_string = str(timedelta(seconds = uptime_seconds))
         return uptime_string
+
+    def __find_segment( self, _list, search_segment ):
+        c = 0
+        for line in _list:
+            if search_segment in line:
+                return _list[ c ]
+            c += 1
+
 
 # End File: app/helpers/pishell.py
